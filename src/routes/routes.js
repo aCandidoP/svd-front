95e64f62608481e2954ad2a7f5ed48e11e2dec9a
Rafael Vicente <rafaelpv15@gmail.com>
2025-06-10 08:46:55 -0300

    refac: corrigido o local do BrowserRouter e adicionado rota Home ao router

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,13 @@
+import Home from '../pages/Home';
 import Login from '../pages/Login';
 
 // Adicionar as rotas aqui
 const routes = [
+  {
+    path: '/',
+    element: <Home />,
+    name: 'Home',
+  },
   {
     path: '/login',
     element: <Login />,
