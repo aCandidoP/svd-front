c50ee0db7449d184a3dce0d55b7792c52b8e91f3
Rafael Vicente <rafaelpv15@gmail.com>
2025-06-10 12:42:37 -0300

    feat: adicionado detalhe chamados

---
+++ src/pages/DetalheChamados/index.jsx
@@ -0,0 +1,7 @@
+export default function DetalheChamados() {
+  return (
+    <div>
+      <h2>Detalhes do chamado</h2>
+    </div>
+  );
+}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,8 @@
+import DetalheChamados from '../pages/DetalheChamados';
 import Home from '../pages/Home';
+import ListarChamados from '../pages/ListarChamados';
 import Login from '../pages/Login';
+import NovoChamado from '../pages/NovoChamado';
 
 // Adicionar as rotas aqui
 const routes = [
@@ -7,6 +10,21 @@ const routes = [
     path: '/',
     element: <Home />,
     name: 'Home',
+  },
+  {
+    path: '/novo-chamado',
+    element: <NovoChamado />,
+    name: 'Novo Chamado',
+  },
+  {
+    path: '/consultar-chamados',
+    element: <ListarChamados />,
+    name: 'Consultar Chamados',
+  },
+  {
+    path: '/detalhes-chamado/:id',
+    element: <DetalheChamados />,
+    name: 'Detalhes do Chamado',
   },
   {
     path: '/login',
