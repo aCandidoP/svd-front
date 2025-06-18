3d864eccfcb0297bc791b8aab87774ad885e9bce
Rafael Vicente <rafaelpv15@gmail.com>
2025-06-18 15:26:27 -0300

    feat: validação do token na App 🔑🔐

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useEffect } from 'react';
+import { redirect } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import { useAuth } from './contexts/AuthContext';
 import AppRouter from './routes/Router';
 
 function App() {
+  // TODO: Criar componente para o load das páginas/componentes
   // Token state to manage authentication
   const { login } = useAuth();
 
@@ -13,6 +15,7 @@ function App() {
     const storedToken = localStorage.getItem('token');
     if (!storedToken) {
       // se storedToken igual a null então redirecionada para a rota de login
+      redirect('/login');
     }
     if (storedToken) {
       login(storedToken);
