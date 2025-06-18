79d05417dce4ffb6256c2d482e68479d2b4cbcf3
Rafael Vicente <rafaelpv15@gmail.com>
2025-06-18 15:27:58 -0300

    feat: criação de um validador para o token(talvez dar um carinho maior nele depois)

--- src/helpers/decode.js
+++ src/helpers/decode.js
@@ -1,8 +1,24 @@
-import { jwtDecode } from "jwt-decode"
+import { jwtDecode } from 'jwt-decode';
 
 export function decodeJwt() {
-    const token = localStorage.getItem('token')
-    const decoded_token = jwtDecode(token)
-    console.log(decoded_token)
+  const token = localStorage.getItem('token');
+  const decoded_token = jwtDecode(token);
+  console.log(decoded_token);
 }
 
+export function validTokenDecoded() {
+  const token = localStorage.getItem('token');
+  console.log(token);
+  if (!token) return false;
+
+  try {
+    const decoded = jwtDecode(token);
+    if (decoded.exp && Date.now() >= decoded.exp * 1000) {
+      return false;
+    }
+    console.log(decoded);
+    return true;
+  } catch (error) {
+    console.log('Token Invalido!');
+  }
+}
