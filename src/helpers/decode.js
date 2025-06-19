20cc3235f4ebf52a8625d181811d07ec94c8c9e5
Rafael Vicente <rafaelpv15@gmail.com>
2025-06-19 11:26:10 -0300

    feat: refatorado o decoded e adicionado a possibilidade de pegar o userId do token

--- src/helpers/decode.js
+++ src/helpers/decode.js
@@ -1,14 +1,15 @@
 import { jwtDecode } from 'jwt-decode';
 
-export function decodeJwt() {
-  const token = localStorage.getItem('token');
+function decodeJwt(token) {
   const decoded_token = jwtDecode(token);
-  console.log(decoded_token);
+  return decoded_token;
 }
 
-export function validTokenDecoded() {
-  const token = localStorage.getItem('token');
-  console.log(token);
+export function getUserIdJwt() {
+  return JSON.parse(decodeJwt().sub).id;
+}
+
+export function validTokenDecoded(token) {
   if (!token) return false;
 
   try {
@@ -16,7 +17,6 @@ export function validTokenDecoded() {
     if (decoded.exp && Date.now() >= decoded.exp * 1000) {
       return false;
     }
-    console.log(decoded);
     return true;
   } catch (error) {
     console.log('Token Invalido!');
