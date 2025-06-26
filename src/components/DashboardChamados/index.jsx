a7a331bbacf628f7a8047983d61387e3b235fb16
Rafael Vicente <rafaelpv15@gmail.com>
2025-06-26 09:21:22 -0300

    feat: integracao da dashboard com o ListarChamados

--- src/components/DashboardChamados/index.jsx
+++ src/components/DashboardChamados/index.jsx
@@ -1,6 +1,43 @@
+import { useCallback, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useAuth } from '../../contexts/AuthContext';
+
+import './styles.css';
 
 export default function DashboardChamados() {
+  const [status, setStatus] = useState({});
+
+  // 'NOVO', 'EM_ATENDIMENTO', 'SOLUCIONADO', 'FECHADO'
+
+  const { token } = useAuth();
+  const fetchStatusCounts = useCallback(async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:5000/chamados/contagem_por_status`,
+        {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const data = await response.json();
+      setStatus(data);
+    } catch (error) {
+      console.error('Erro: ', error);
+    }
+  }, [token]);
+
+  useEffect(() => {
+    let isMounted = true;
+    if (isMounted) {
+      fetchStatusCounts();
+    }
+    // Cleanup function to avoid memory leaks
+    return () => {
+      isMounted = false;
+    };
+  }, [fetchStatusCounts]);
   return (
     <div
       className="container d-flex justify-content-center  flex-column"
@@ -19,69 +56,83 @@ export default function DashboardChamados() {
           + Novo Chamado
         </Link>
       </div>
-      <div className="d-flex pt=2 pb-2 align-items-center justify-content-between border">
-        <div className="d-flex justify-content-center align-items-center gap-1 ms-2">
-          <i className="bi bi-check-lg"></i>
-          Chamados aguardando sua aprovação
+      <Link to={`/consultar-chamados?status=AGUARDANDO`}>
+        <div className="d-flex pt=2 pb-2 align-items-center justify-content-between border hover">
+          <div className="d-flex justify-content-center align-items-center gap-1 ms-2">
+            <i className="bi bi-check-lg"></i>
+            Chamados aguardando sua aprovação
+          </div>
+          <div className="me-2 ps-2 pe-2 bg-secondary rounded text-white mt-2">
+            {status.AGUARDANDO || 0}
+          </div>
         </div>
-        <div className="me-2 ps-2 pe-2 bg-secondary rounded text-white mt-2">
-          0
+      </Link>
+      <Link to={`/consultar-chamados?status=NOVO`}>
+        <div className="d-flex pt=2 pb-2 align-items-center justify-content-between border hover">
+          <div className="d-flex justify-content-center align-items-center gap-1 ms-2">
+            <i className="bi bi-circle-fill text-success"></i>
+            Novo
+          </div>
+          <div className="me-2 ps-2 pe-2 bg-secondary rounded text-white mt-2">
+            {status.NOVO || 0}
+          </div>
         </div>
-      </div>
-      <div className="d-flex pt=2 pb-2 align-items-center justify-content-between border">
-        <div className="d-flex justify-content-center align-items-center gap-1 ms-2">
-          <i className="bi bi-circle-fill text-success"></i>
-          Novo
+      </Link>
+      <Link to={`/consultar-chamados?status=EM_ATENDIMENTO`}>
+        <div className="d-flex pt=2 pb-2 align-items-center justify-content-between border hover">
+          <div className="d-flex justify-content-center align-items-center gap-1 ms-2">
+            <i className="bi bi-circle text-success"></i>
+            Em atendimento
+          </div>
+          <div className="me-2 ps-2 pe-2 bg-secondary rounded text-white mt-2">
+            {status.EM_ATENDIMENTO || 0}
+          </div>
         </div>
-        <div className="me-2 ps-2 pe-2 bg-secondary rounded text-white mt-2">
-          0
+      </Link>
+      <Link to={`/consultar-chamados?status=PENDENTE`}>
+        <div className="d-flex pt=2 pb-2 align-items-center justify-content-between border hover">
+          <div className="d-flex justify-content-center align-items-center gap-1 ms-2">
+            <i className="bi bi-circle-fill text-warning"></i>
+            Pendente
+          </div>
+          <div className="me-2 ps-2 pe-2 bg-secondary rounded text-white mt-2">
+            {status.PENDENTE || 0}
+          </div>
         </div>
-      </div>
-      <div className="d-flex pt=2 pb-2 align-items-center justify-content-between border">
-        <div className="d-flex justify-content-center align-items-center gap-1 ms-2">
-          <i className="bi bi-circle text-success"></i>
-          Em atendimento
+      </Link>
+      <Link to={`/consultar-chamados?status=SOLUCIONADO`}>
+        <div className="d-flex pt=2 pb-2 align-items-center justify-content-between border hover">
+          <div className="d-flex justify-content-center align-items-center gap-1 ms-2">
+            <i className="bi bi-circle"></i>
+            Solucionado
+          </div>
+          <div className="me-2 ps-2 pe-2 bg-secondary rounded text-white mt-2">
+            {status.SOLUCIONADO || 0}
+          </div>
         </div>
-        <div className="me-2 ps-2 pe-2 bg-secondary rounded text-white mt-2">
-          0
+      </Link>
+      <Link to={`/consultar-chamados?status=FECHADO`}>
+        <div className="d-flex pt=2 pb-2 align-items-center justify-content-between border hover">
+          <div className="d-flex justify-content-center align-items-center gap-1 ms-2">
+            <i className="bi bi-circle-fill"></i>
+            Fechado
+          </div>
+          <div className="me-2 ps-2 pe-2 bg-secondary rounded text-white mt-2">
+            {status.FECHADO || 0}
+          </div>
         </div>
-      </div>
-      <div className="d-flex pt=2 pb-2 align-items-center justify-content-between border">
-        <div className="d-flex justify-content-center align-items-center gap-1 ms-2">
-          <i className="bi bi-circle-fill text-warning"></i>
-          Pendente
+      </Link>
+      <Link to={`/consultar-chamados?status=EXCLUIDO`}>
+        <div className="d-flex pt=2 pb-2 align-items-center justify-content-between border hover">
+          <div className="d-flex justify-content-center align-items-center gap-1 ms-2">
+            <i className="bi bi-trash3-fill text-danger"></i>
+            Excluído
+          </div>
+          <div className="me-2 ps-2 pe-2 bg-secondary rounded text-white mt-2">
+            {status.EXCLUIDO || 0}
+          </div>
         </div>
-        <div className="me-2 ps-2 pe-2 bg-secondary rounded text-white mt-2">
-          0
-        </div>
-      </div>
-      <div className="d-flex pt=2 pb-2 align-items-center justify-content-between border">
-        <div className="d-flex justify-content-center align-items-center gap-1 ms-2">
-          <i className="bi bi-circle"></i>
-          Solucionado
-        </div>
-        <div className="me-2 ps-2 pe-2 bg-secondary rounded text-white mt-2">
-          0
-        </div>
-      </div>
-      <div className="d-flex pt=2 pb-2 align-items-center justify-content-between border">
-        <div className="d-flex justify-content-center align-items-center gap-1 ms-2">
-          <i className="bi bi-circle-fill"></i>
-          Fechado
-        </div>
-        <div className="me-2 ps-2 pe-2 bg-secondary rounded text-white mt-2">
-          0
-        </div>
-      </div>
-      <div className="d-flex pt=2 pb-2 align-items-center justify-content-between border">
-        <div className="d-flex justify-content-center align-items-center gap-1 ms-2">
-          <i className="bi bi-trash3-fill text-danger"></i>
-          Excluído
-        </div>
-        <div className="me-2 ps-2 pe-2 bg-secondary rounded text-white mt-2">
-          0
-        </div>
-      </div>
+      </Link>
     </div>
   );
 }
--- src/pages/ListarChamados/index.jsx
+++ src/pages/ListarChamados/index.jsx
@@ -1,4 +1,5 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import CardChamado from '../../components/CardChamado';
 import Paginacao from '../../components/Paginacao';
 import Spinner from '../../components/spinner';
@@ -11,15 +12,33 @@ export default function ListarChamados() {
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(5);
   const [perPage, setPerPage] = useState(5);
-  // para validar depois
-  const { token, logout } = useAuth();
+  const [status, setStatus] = useState(null);
+  const [hasFetched, setHasFetched] = useState(false);
 
-  // Retirado a possibilidade de recriação da função desnecessariamente
+  const { token, logout } = useAuth();
+  const [searchParams] = useSearchParams();
+  // pega o parametro status da url
+  const statusParam = searchParams.get('status');
+
+  const listStatus = useMemo(
+    () => [
+      'NOVO',
+      'EM_ATENDIMENTO',
+      'SOLUCIONADO',
+      'FECHADO',
+      'PENDENTE',
+      'EXCLUIDO',
+      'AGUARDANDO',
+    ],
+    []
+  );
+
   const getChamadosPaginados = useCallback(async () => {
     try {
       setLoading(true);
+      setHasFetched(false);
       const response = await fetch(
-        `http://localhost:5000/chamados/paginados?page=${page}&per_page=${perPage}`,
+        `http://localhost:5000/chamados/paginados/${status}?page=${page}&per_page=${perPage}`,
         {
           method: 'GET',
           headers: {
@@ -29,18 +48,29 @@ export default function ListarChamados() {
       );
 
       const data = await response.json();
-      // ver retorno correto da api
-      setChamados(data.chamados);
-      setPage(data.pagination_metadata.page);
-      setTotalPages(data.pagination_metadata.total_pages);
+      console.log(data);
+      setChamados(data.chamados || []);
+      setPage(data.pagination_metadata?.page || 1);
+      setTotalPages(data.pagination_metadata?.total_pages || 1);
+      setHasFetched(true);
     } catch (error) {
       console.error(error);
+      setChamados([]);
+      setHasFetched(true);
     } finally {
       setLoading(false);
     }
-  }, [perPage, page, token]);
+  }, [perPage, page, token, status]);
 
-  // Paginacao
+  useEffect(() => {
+    // verifica se o status já foi definido, se não força a definição
+    if (status === null) {
+      const initialStatus =
+        statusParam && listStatus.includes(statusParam) ? statusParam : 'NOVO';
+      setStatus(initialStatus);
+    }
+  }, [statusParam, status, listStatus]);
+
   useEffect(() => {
     let isMounted = true;
     if (!validTokenDecoded(token)) {
@@ -48,16 +78,19 @@ export default function ListarChamados() {
       return;
     }
     async function fetchData() {
-      if (isMounted) {
-        getChamadosPaginados();
+      if (isMounted && status !== null) {
+        await getChamadosPaginados();
       }
     }
     fetchData();
-    // cleanup
     return () => {
       isMounted = false;
     };
-  }, [getChamadosPaginados, token, logout]);
+  }, [getChamadosPaginados, token, logout, status]);
+
+  if (status === null) {
+    return <Spinner />;
+  }
 
   return (
     <div className="container">
@@ -66,7 +99,23 @@ export default function ListarChamados() {
         <Spinner />
       ) : (
         <>
-          {chamados.length > 0 ? (
+          <select
+            name="status"
+            id="status"
+            className="form-select mb-3"
+            value={status}
+            onChange={(e) => setStatus(e.target.value)}
+          >
+            {listStatus.map((statusOption, index) => (
+              <option value={statusOption} key={index}>
+                {statusOption}
+              </option>
+            ))}
+          </select>
+
+          {!hasFetched ? (
+            <Spinner />
+          ) : chamados.length > 0 ? (
             <div className="container">
               <ul>
                 {chamados.map((chamado) => (
@@ -84,7 +133,9 @@ export default function ListarChamados() {
               />
             </div>
           ) : (
-            'Nenhum chamado encontrado.'
+            <div className="alert alert-info">
+              Nenhum chamado encontrado para o status {status}.
+            </div>
           )}
         </>
       )}
